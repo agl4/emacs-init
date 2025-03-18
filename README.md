c5708d7f685b2bc01ad377d6b5585cddf2ae584c
Attila GOLONCSER <agl@kxxp.net>
2025-03-18 17:00:02 +0100

    Initial version with fixes

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# emacs-installer
+# emacs-init
+
+Install my [chemacs-based](https://github.com/plexus/chemacs2) environment.
+
+Supported platforms:
+
+- Ubuntu
+- Fedora
+- macOS
+
+
--- share/chemacs-profiles/.emacs-profiles.el
+++ share/chemacs-profiles/.emacs-profiles.el
@@ -1,3 +1,3 @@
 (("emacs-emacs.d" . ((user-emacs-directory . "~/src/emacs/agl4/emacs-emacs.d")))
- ("doom" . ((user-emacs-directory . "~/src/emacs/hlissner/doom-emacs.git")))
- ("spacemacs" . ((user-emacs-directory . "~/src/emacs/syl20bnr/spacemacs.git"))))
+ ("doom" . ((user-emacs-directory . "~/src/emacs/hlissner/doom-emacs")))
+ ("spacemacs" . ((user-emacs-directory . "~/src/emacs/syl20bnr/spacemacs"))))
