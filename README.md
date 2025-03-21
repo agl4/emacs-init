4bfc7e02f40ab491eda3b4326e3c8c44f68c720f
Attila GOLONCSER <agl@kxxp.net>
2025-03-21 12:33:19 +0100

    Add the bootstrap scripts

--- README.md
+++ README.md
@@ -8,4 +8,20 @@ Supported platforms:
 - Fedora
 - macOS
 
+## Prerequisites
+
+Run `bootstrap.sh`, which installs the dependencies.
+
+```shell
+curl -fsSL https://raw.githubusercontent.com/agl4/emacs-init/refs/heads/main/bootstrap.sh | sh
+```
+
+## Install
+
+To install the environment run the following:
+
+```shell
+git clone https://github.com/agl4/emacs-init && make install
+```
+
 
--- bootstrap.sh
+++ bootstrap.sh
@@ -1,59 +1,56 @@
-#!/usr/bin/env bash
+#!/bin/sh
 
 set -eu
 
-echo "Bootstrapping system..."
+main(){
+    echo "Bootstrapping system..."
 
-case $(uname -s) in
-    Linux)
-        . /etc/os-release
-        if [ -z "$ID" ] ; then
-            echo "ERROR: Cannot detect distro."
-        fi
-        [[ "$ID" == "raspbian"  ]] && {
-            sudo locale-gen en_US.UTF-8
-            sudo update-locale LANG=en_US.UTF-8
-        }
-        case $ID in
-            ubuntu|debian|raspbian)
-                [[ -x /usr/bin/sudo ]] || {
-                    apt-get update
-                    apt-get install -y sudo
-                }
-                sudo apt-get update
-                sudo apt-get install -y git make
+    case $(uname -s) in
+        Linux)
+            . /etc/os-release
+            if [ -z "$ID" ] ; then
+                echo "ERROR: Cannot detect distro."
+            fi
+            if [ "$ID" = "raspbian"  ] ; then
+                sudo locale-gen en_US.UTF-8
+                sudo update-locale LANG=en_US.UTF-8
+            fi
+            case $ID in
+                ubuntu|debian|raspbian)
+                    if [ ! -x /usr/bin/sudo ] ; then
+                        apt-get update
+                        apt-get install -y sudo
+                    fi
+                    sudo apt-get update
+                    sudo apt-get install -y git make curl unzip
+                ;;
+                fedora)
+                    sudo dnf install -y git make curl unzip
+                ;;
+                opensuse*)
+                    if [ -x /usr/bin/sudo ] ; then
+                        zypper install -y sudo
+                    fi
+                    sudo zypper install -y git make curl unzip
+                ;;
+                *) echo "Unsupported distro: $ID"
+                exit 1
+                ;;
+            esac
             ;;
-            fedora)
-                sudo dnf install -y git make
+        Darwin)
+            xcode-select --install || true
             ;;
-            opensuse*)
-                [[ -x /usr/bin/sudo ]] || {
-                    zypper install -y sudo
-                }
-                sudo zypper install -y git make sudo
+        FreeBSD)
+            export ASSUME_ALWAYS_YES=YES
+            pkg update -f
+            pkg install -y git gmake curl unzip sudo bash
             ;;
-            *) echo "Unsupported distro: $ID"
-               exit 1
-               ;;
-        esac
-        ;;
-    Darwin)
-        xcode-select --install || true
-        ;;
-    FreeBSD)
-        export ASSUME_ALWAYS_YES=YES
-        pkg update -f
-        pkg install -y git sudo mosh python fish tmux gmake
-        ;;
-    *)
-        echo "Unsupported OS."
-        exit 1
-        ;;
-esac
+        *)
+            echo "Unsupported OS."
+            exit 1
+            ;;
+    esac
+}
 
-cat <<EOF
-Now run:
-
-    make
-
-EOF
+main
