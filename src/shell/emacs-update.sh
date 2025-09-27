81a7db87104dedfaeb68fd3d52b399f85b556e7f
Attila GOLONCSER <agl@glnc.hu>
2025-09-27 15:59:11 +0200

    fix: Simplify script

--- src/shell/emacs-update.sh
+++ src/shell/emacs-update.sh
@@ -2,14 +2,7 @@
 
 set -eu
 
-for operation in pull ; do
-    while read -r repo_path ; do
-            echo "**** $operation $(basename "$repo_path") ******"
-            git -C "$repo_path" "$operation"
-    done < <(ghq  list -p | grep '\/emacs\/')
-done
-
-dir=$(ghq list -p | grep 'emacs-emacs.d')
-
-cd "$dir" && make update
-
+while read -r repo_path ; do
+    echo "**** $operation "$(basename "$repo_path")" ******"
+    git -C "$repo_path" pull
+done < <(ghq  list -p | grep '\/emacs\/')
